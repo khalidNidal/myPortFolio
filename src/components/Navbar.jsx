9277c082ea8842c7a194dd23857c0350d5c87b5e
Khalid Nidal Saidi <137891091+khalidNidal@users.noreply.github.com>
2024-01-10 01:37:45 +0200

    logoProblem

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={"../../public/logo.png"} alt='logo' className='w-9 h-9 object-contain' />
+          <img src={"public/logo.png"} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer  '>
              Khalid Nidal&nbsp; 
             <span className='sm:block hidden'> | Front-End develpoer</span>
